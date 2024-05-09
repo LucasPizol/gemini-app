7d6027d9c0b8b3f7e0c52c15f5843b2827c83fe5
Lucas Pizol Ferreira <lucaspizolfe@gmail.com>
2024-05-09 18:00:38 -0300

    feat: initial commit

---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,100 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:google_generative_ai/google_generative_ai.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late final GenerativeModel model;
+  String prompt = "";
+  String answer = '';
+  bool loading = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    var key = "";
+
+    model = GenerativeModel(model: "gemini-1.0-pro", apiKey: key);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              const Center(
+                child: Column(children: [
+                  Text("Seja bem vindo à Gemini!"),
+                  Text("Aqui você vai ver as informações sobre o Gemini!"),
+                ]),
+              ),
+              Expanded(
+                  child: Container(
+                padding: const EdgeInsets.all(20),
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  border: Border.all(),
+                ),
+                child: MarkdownBody(
+                  data: answer,
+                ),
+              )),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      decoration: const InputDecoration(
+                          hintText: "Pergunta", border: OutlineInputBorder()),
+                      onChanged: (value) {
+                        prompt = value;
+                        setState(() {});
+                      },
+                    ),
+                  ),
+                  ElevatedButton(
+                      style: ButtonStyle(
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(4),
+                                      side: BorderSide(color: Color.fromARGB(255, 122, 120, 255))))),
+                      onPressed: () async {
+                        if (prompt.isEmpty) {
+                          return;
+                        }
+
+                        loading = true;
+                        setState(() {});
+
+                        var response =
+                            await model.generateContent([Content.text(prompt)]);
+
+                        if (response.text != null) {
+                          answer = response.text!;
+                        }
+
+                        loading = false;
+                        setState(() {});
+                      },
+                      child: Text("Enviar"))
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
